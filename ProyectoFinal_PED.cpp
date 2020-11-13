87f64ae1f8f4af9e8c10cf14fcaee35b75cbd95b
RivasDiego <00121520@uca.edu.sv>
2020-11-13 11:53:27 -0600

    Base

---
+++ ProyectoFinal_PED.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <string.h>
+
+using namespace std;
+
+/* 
+Oso meloso
+Crepas a $3.50
+Pie/chesse cake con café a $4.00
+Pastel entero, sabor al gusto del cliente a $30.00
+
+McBurger
+Orden de papas $1.00
+Hamburguesa normal $3.50
+Hamburguesa doble $5.00
+Orden de nachos $4.50
+chucho caliente  $1.25
+
+El buen café
+Malteadas $2.00
+Capuccino $2.50
+Café $1.25
+Bolsa grande de grano de café $10.00
+*/
+struct product {
+    string pepa;
+
+};
+
+int main (){
+
+}
